8b9bd569ac971e2e60a3fc33759e267e7bc541dd
Kelsey Chapman <kelseyc.chapman@gmail.com>
2016-10-03 16:46:05 -0600

    tesst 4 pass

--- routes/session.js
+++ routes/session.js
@@ -1,10 +1,56 @@
 'use strict';
 
+const bcrypt = require('bcrypt-as-promised');
+const boom = require('boom');
 const express = require('express');
+const knex = require('../knex');
+const humps = require('humps');
 
-// eslint-disable-next-line new-cap
 const router = express.Router();
 
-// YOUR CODE HERE
+
+router.get('/users', (req, res) => {
+  if (req.session.userId) {
+    return res.send(true);
+  }
+  res.send(false)
+})
+
+router.post('/users', (req, res, next) => {
+  if (!req.body.email || !req.body.email.trim()) {
+    return next(boom.create(400, 'Email must not be blank'));
+  }
+  if (!req.body.password || req.body.password.length < 8) {
+    return next(boom.create(400, 'Password must not be blank'));
+  }
+  let user;
+
+  knex('users')
+    .where('email', req.body.email)
+    .first()
+    .then((row) => {
+      if (!row) {
+        throw boom.create(400, 'Bad email or password');
+      }
+      user = humps.camelizeKeys(row)
+      return bcrypt.compare(req.body.password, user.hashedPassword);
+    })
+    .then(() => {
+      delete user.hashedPassword
+      req.session.userId = user.id
+      res.send(user)
+    })
+    .catch(bcrypt.MISMATCH_ERROR, () => {
+      throw boom.create(400, 'Bad email or password');
+    })
+    .catch((err) => {
+      next(err);
+    });
+})
+
+router.delete('/users', (req, res) => {
+  req.session = null;
+  res.send(true);
+})
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,10 @@
-
 'use strict';
 
 const bcrypt = require('bcrypt-as-promised');
 const boom = require('boom');
 const express = require('express');
-const jwt = require('jsonwebtoken');
 const knex = require('../knex');
 const { camelizeKeys, decamelizeKeys } = require('humps');
-
-// eslint-disable-next-line new-cap
 const router = express.Router();
 
 router.post('/users', (req, res, next) => {
